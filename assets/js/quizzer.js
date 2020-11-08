3179187bc1bea87c1b53a0c40c5f10a4bb18abe2
Giamo Lao <giamolao@paradoxicalsw.com>
2020-11-08 11:16:25 -0500

    Added questions

--- assets/js/quizzer.js
+++ assets/js/quizzer.js
@@ -1,4 +1,54 @@
+// Define array of 10 questions
 let questions =
-{
-
-}
+[
+  {
+    "q": "What keyword is used to declare a variable?",
+    "o": ["const", "var", "let", "all of the above"],
+    "a": 3 // o[3] is the answer
+  },
+  {
+    "q": "What is the correct syntax to print a page?",
+    "o": ["browser.print()", "navigator.print()", "window.print()", "document.print()"],
+    "a": 2 // o[2] is the answer
+  },
+  {
+    "q": "Which built-in method removes the last element from an array and returns it?",
+    "o": ["last()", "pop()", "get()", "none of the above"],
+    "a": 1 // o[1] is the answer
+  },
+  {
+    "q": "Which built-in method reverses the order of the elements of an array?",
+    "o": ["reverse()", "flip()", "zToA()", "mirror()"],
+    "a": 0 // o[0] is the answer
+  },
+  {
+    "q": "Which of the following function of String object extracts a section of a string and returns a new string?",
+    "o": ["split()", "slice()", "section()", "cut()"],
+    "a": 1 // o[1] is the answer
+  },
+  {
+    "q": "What is the HTML tag under which one can write the JavaScript code?",
+    "o": ["<javascript>", "<script>", "<js>", "<code>"],
+    "a": 1 // o[1] is the answer
+  },
+  {
+    "q": "What method is used to display an alert box?",
+    "o": ["popUp()", "display()", "alert()", "log()"],
+    "a": 2 // o[2] is the answer
+  },
+  {
+    "q": "Inside of the <script> tag, how do you link to an external .js file?",
+    "o": ["src=\"<filepath>\"", "href=\"<filepath>\"", "ref=\"<filepath>\"", "path=\"<filepath>\""],
+    "a": 0 // option 1 is the answer
+  },
+  {
+    "q": "for (var x = 0; x <= 5; x++) { window.alert(\"Hello\"); }\nHow many time's will an alert window pop up?",
+    "o": ["1", "4", "5", "6"],
+    "a": 3 // o[3] is the answer
+  },
+  {
+    "q": "var x = 0; if (x == 0) { let x = 10; } console.log(x) \nWhat is logged to the console?",
+    "o": ["10", "0", "5", "error"],
+    "a": 1 // o[1] is the answer
+  }
+]
